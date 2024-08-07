a4afd8c80955c3ba12b1fdf4178921af00f094f7
Kaustuv <Kaustuv-Karki>
2024-08-08 01:34:47 +0545

    testing the tailwinf

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import React from 'react';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className='text-[3rem] text-white'>This is the text</h1>
+    </main>
   );
 }
