41592279e6d77ec159af6f9f9583757a5be70e6e
Kaustuv Karki <karkikaustuv@gmail.com>
2024-08-09 00:23:28 +0545

    themes and shadcn installation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
+import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 import { useState } from 'react';
 import React from 'react';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className='text-[3rem] text-white'>This is the text</h1>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
+      <h1 className="text-[3rem] ">This is the text</h1>
+      <Button>Click here</Button>
     </main>
   );
 }
