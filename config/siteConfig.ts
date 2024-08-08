0bc0b99e5d53246fc018feface505279a31511b8
Kaustuv Karki <karkikaustuv@gmail.com>
2024-08-08 22:48:30 +0545

    Layout setup

---
+++ config/siteConfig.ts
@@ -0,0 +1,12 @@
+import { type SiteConfig } from '../types/site';
+
+export const siteConfig: SiteConfig = {
+  name: 'LMS App',
+  description: 'A Learning Management System',
+  url: 'https://lmsapp.com',
+  ogImage: '/og-image.jpg',
+  links: {
+    twitter: 'https://twitter.com/lmsapp',
+    github: ''
+  }
+};
